20574da800e46225fa10068783699c3bf89e5041
heitorlucas2018 <heitorl09@gmail.com>
2020-07-26 13:03:49 -0300

    build and deploy

---
+++ out/_next/static/5CF29CHRdBPX3S7DokHid/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F970a5561a9a7e6da0c14.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- out/_next/static/BfrQIsKJvP0Jeu1pEqrQN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002Ff4b69c7184c7b8b925b3.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
