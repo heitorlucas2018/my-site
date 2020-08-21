1f517a23710477b0c467af711e381f04bcd7678b
heitorlucas2018 <heitorl09@gmail.com>
2020-08-21 12:41:55 -0300

    deploy

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,9 +5,9 @@ const data = {
     {
       title: "#Techskills", data: [
         { title: "Programming Language:", data: ["Php", "Java", "Pl/SQL", "JavaScript"] },
-        { title: "Frameworks:", data: ["Laravel", "Spring Boot", "React js", "Next js", "React Native",] },
+        { title: "Frameworks:", data: ["Laravel", "Spring Boot", "React js", "Next js", "React Native","Jest"] },
         { title: "Database:", data: ["Mysql", "Oracle DB 11g", "Firebase", "Redis"] },
-        { title: "Tools and Technologies:", data: ["Mysql", "Oracle DB 11g", "Firebase", "Redis"] }
+        { title: "Tools and Technologies:", data: ["HTML5", "Jest", "Nginx", "Apache"] }
       ]
     },
     {
@@ -19,24 +19,25 @@ const data = {
         { title: "Esforçado", data: [] },
         { title: "Analítico", data: [] },
         { title: "Detalhista", data: [] },
+        { title: "Pragmátivo", data: [] },
       ]
     },
     {
       title: "#Experiences:", data: [
         {
           title: "QI.Jobs ​— S​tartup Weekend 2019",
-          tags: ["App Mobile", "React Native"],
+          tags: ["App Mobile", "React Native", "Jest", "Firebase"],
           description: "Projeto montado com o objetivo de levar para as instituições, candidatos tendo perfil ideal para a vaga, assim como conduzir o candidato a vaga, que este cumpra seus requisitos, sendo match perfeito."
         },{
           title: "Desenvolvedor ​— Próton Sistemas",
           description: "Próton Sistemas é uma software house com sistemas para administração de estoque, gestão empresarial, frente de loja e outros.",
         },{
-          title: "Extrega Expressa",
+          title: "Entrega Expressa",
           tags: ["API Rest", "PHP", "Laravel", "Mysql", "Redis"],
           description: "Esta é uma API que esta sendo utilizada por uma plataforma de entrega. Por se tratar de uma plataforma de uso exclusivo da empresa, não pode ser passado muitos detalhes.",
         },{
           title: "Perambular - Startup",
-          tags: ["API Rest", "PHP", "Laravel", "Mysql", "Firebase", "Vue.js", "React Native"],
+          tags: ["App Mobile", "API Rest", "PHP", "Laravel", "Mysql", "Firebase", "Vue.js", "React Native", "Docker"],
           description: "Esta é uma plataforma de agendamento digital, que diferente de todas as outras plataformas de agendamento, não é focada para apenas um tipo de ambiente mas para diversas, assim como outras plataformas já vem fazendo com restaurantes e processos de entregas e coletas.",
         },
       ]
