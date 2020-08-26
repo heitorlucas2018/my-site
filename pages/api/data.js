50f3c07129973f5d01aac6cedf8259ec71efaa1d
heitorlucas2018 <heitorl09@gmail.com>
2020-08-26 11:30:45 -0300

    projetos

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,7 +5,7 @@ const data = {
     {
       title: "#Techskills", data: [
         { title: "Programming Language:", data: ["Php", "Java", "Pl/SQL", "JavaScript"] },
-        { title: "Frameworks:", data: ["Laravel", "Spring Boot", "React js", "Next js", "React Native","Jest"] },
+        { title: "Frameworks:", data: ["Laravel", "Spring Boot", "React js", "Next js", "React Native", "Jest"] },
         { title: "Database:", data: ["Mysql", "Oracle DB 11g", "Firebase", "Redis"] },
         { title: "Tools and Technologies:", data: ["HTML5", "Jest", "Nginx", "Apache"] }
       ]
@@ -23,19 +23,23 @@ const data = {
       ]
     },
     {
-      title: "#Experiences:", data: [
+      title: "#Portfolio:", data: [
         {
           title: "QI.Jobs ​— S​tartup Weekend 2019",
           tags: ["App Mobile", "React Native", "Jest", "Firebase"],
           description: "Projeto montado com o objetivo de levar para as instituições, candidatos tendo perfil ideal para a vaga, assim como conduzir o candidato a vaga, que este cumpra seus requisitos, sendo match perfeito."
-        },{
+        }, {
           title: "Desenvolvedor ​— Próton Sistemas",
           description: "Próton Sistemas é uma software house com sistemas para administração de estoque, gestão empresarial, frente de loja e outros.",
-        },{
+        }, {
           title: "Entrega Expressa",
           tags: ["API Rest", "PHP", "Laravel", "Mysql", "Redis"],
           description: "Esta é uma API que esta sendo utilizada por uma plataforma de entrega. Por se tratar de uma plataforma de uso exclusivo da empresa, não pode ser passado muitos detalhes.",
-        },{
+        }, {
+          title: "Gestao de Frotas",
+          tags: ["App Mobile", "API Rest", "PHP", "Laravel", "Mysql", "Firebase", "Vue.js", "React Native", "Docker", "CI/CD"],
+          description: "Esta é uma ferramenta utilizada para monitorar frota de entrega ou coleta do produto, com esta ferramenta a base de operações logísticas, tem uma visão em tempo real de seus veículos além dos dados de suas entregas, coletas, devoluções entre outras particularidades. ",
+        }, {
           title: "Perambular - Startup",
           tags: ["App Mobile", "API Rest", "PHP", "Laravel", "Mysql", "Firebase", "Vue.js", "React Native", "Docker"],
           description: "Esta é uma plataforma de agendamento digital, que diferente de todas as outras plataformas de agendamento, não é focada para apenas um tipo de ambiente mas para diversas, assim como outras plataformas já vem fazendo com restaurantes e processos de entregas e coletas.",
